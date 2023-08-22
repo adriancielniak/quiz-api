10fdae9d043cb54f2274f1069adf7d1a225d5c04
adriancielniak <adriancielniak@gmail.com>
2023-08-22 13:42:35 +0200

    Remove unnecessary code from answer

--- src/answer/answer.service.spec.ts
+++ src/answer/answer.service.spec.ts
@@ -1,9 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
-import { Question } from 'src/question/entities/question.entity';
-import { Quiz } from 'src/quiz/entities/quiz.entity';
-import { Any, DataSource, DeepPartial, Repository } from 'typeorm';
-import { AnswerModule } from './answer.module';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { AnswerService } from './answer.service';
 import { Answer } from './entities/answer.entity';
 
@@ -11,7 +8,6 @@ import { Answer } from './entities/answer.entity';
 describe('AnswerService', () => {
   let service: AnswerService;
   let answerRepository: Repository<Answer>;
-  //let dataSource: DataSource;
   
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -23,8 +19,6 @@ describe('AnswerService', () => {
 
     service = module.get<AnswerService>(AnswerService)
     answerRepository = module.get<Repository<Answer>>(getRepositoryToken(Answer));
-    //dataSource = module.get<DataSource>(DataSource)
-
   });
 
   it('should be defined', () => {
@@ -60,14 +54,6 @@ describe('AnswerService', () => {
 
   
   describe('getAnswers', () => {
-    /*
-    const firstAnswerInput = {
-      answer_content: "content",
-      priority: 1,
-      is_correct: null
-    }
-    */
-
     const firstAnswer = {
       id: expect.any(Number),
       answer_content: "content",
@@ -89,14 +75,6 @@ describe('AnswerService', () => {
 
       expect(result).toEqual(expect.arrayContaining([firstAnswer]))
     })
-
-    /*
-    const secondAnswerInput = {
-      answer_content: "content 2",
-      priority: null,
-      is_correct: true
-    }
-    */
 
     const secondAnswer = {
       id: expect.any(Number),

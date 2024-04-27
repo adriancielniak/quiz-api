45b73ee75a1a4166c6097cd14e73579a3c02d0fd
adriancielniak <adriancielniak@gmail.com>
2024-04-27 14:19:48 +0200

    Add react dom

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,19 +1,20 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-import { ApolloProvider } from '@apollo/client/react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client/react';;
 
 const client = new ApolloClient({
   uri: 'http://localhost:3000/graphql', 
   cache: new InMemoryCache(),
 });
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(
   <ApolloProvider client={client}>
     <App />
-  </ApolloProvider>,
-  document.getElementById('root')
+  </ApolloProvider>
 );
 
 
